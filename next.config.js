ff84c3d006dadfc3757090ff874479931bad21d7
roboflover <96958044+roboflover@users.noreply.github.com>
2024-06-13 13:54:22 +0300

    mmm

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
     images: {
       domains: ['storage.yandexcloud.net'],
     },
-  };
-  
+    apps: [{
+      name: "app1",
+      script: "node_modules/next/dist/bin/next",
+      //args: "start"
+    }]
+};
