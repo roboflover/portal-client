6b5e22bc7f7a4d184e04b928daff39fea7b0cd22
roboflover <96958044+roboflover@users.noreply.github.com>
2024-06-13 22:49:37 +0300

    privet

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ module.exports = {
     images: {
       domains: ['storage.yandexcloud.net'],
     },
-    apps: [{
-      name: "app1",
-      script: "node_modules/next/dist/bin/next",
-      //args: "start"
-    }]
-};
+  };
+  
+  
