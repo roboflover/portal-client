b03745f2c751906757a953f8fea20d2696d15efd
roboflover <96958044+roboflover@users.noreply.github.com>
2024-06-19 23:12:26 +0300

    ss

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,24 @@ export default function RootLayout({children}: Readonly<{children: React.ReactNo
   
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <title>Робоцветы</title>
+      <head><title>Робоцветы</title>
+      <!-- Yandex.Metrika counter -->
+      <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(97622142, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+        });
+      </script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/97622142" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+      <!-- /Yandex.Metrika counter -->
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           {children}
